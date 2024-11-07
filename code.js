f084a612fdf0e03bd2ac889972b835fc04c9bf54
pquauke <philipp.quauke@gmail.com>
2024-11-07 06:38:36 +0100

    Finish the tasks
    to have a final version for users

--- code.js
+++ code.js
@@ -1,38 +1,39 @@
+const body = document.querySelector("body");
 const container = document.querySelector(".container");
-let gridSize = 50;
+let gridSize = 64;
 let paddingSize = 900 / gridSize / 2;
 
 function createGrid() {
-    for(let j = 0; j < gridSize; j++) {
+    for (let j = 0; j < gridSize; j++) {
         const divVertical = document.createElement("div");
-        divVertical.classList.add("vertical");
-        container.appendChild(divVertical)
-        for(let i = 0; i < gridSize; i++) {
+        container.appendChild(divVertical);
+        for (let i = 0; i < gridSize; i++) {
             const divHorizontal = document.createElement("div");
             divHorizontal.classList.add("horizontal");
-            divVertical.appendChild(divHorizontal)
+            divHorizontal.style.padding = paddingSize + "px";
+            divHorizontal.addEventListener("mouseenter", () => {
+                divHorizontal.style.backgroundColor = "brown";
+            });
+            divVertical.appendChild(divHorizontal);
         }
     }
 }
 
+function removeGrid() {
+    Array.from(container.children).forEach((child) => {
+        container.removeChild(child);
+    });
+}
+
 createGrid();
-const divToBeChanged = document.querySelectorAll("div.horizontal");
-divToBeChanged.forEach((square) => {
-    square.style.padding = paddingSize + "px";
-    square.addEventListener("mouseenter", () => {
-        square.style.backgroundColor = "brown";
-    })
-})
 
 const buttonGridSize = document.createElement("button");
 buttonGridSize.textContent = "Start Again";
 buttonGridSize.addEventListener("click", () => {
-    gridSize = prompt("What grid size do you want?","64");
+    gridSize = prompt("What grid size do you want?", "64");
     paddingSize = 900 / gridSize / 2;
-    const tbr = document.querySelectorAll("div");
-    container.removeChild(tbr);
+    removeGrid();
     createGrid();
+});
 
-})
-container.appendChild(buttonGridSize)
-
+body.appendChild(buttonGridSize);
